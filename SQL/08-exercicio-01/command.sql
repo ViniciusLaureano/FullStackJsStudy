2e9e7520c5501706262fc42f0959d93e53236972
ViniciusLaureano <viniciusglaureano@gmail.com>
2025-01-21 19:57:26 -0300

    aula 08 + 09

---
+++ SQL/08-exercicio-01/command.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE db_burguer_queen;
+
+CREATE TABLE IF NOT clientes(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone VARCHAR(20),
+  address VARCHAR(255),
+  created_at DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE IF NOT EXISTS suppliers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  phone VARCHAR(20),
+  email VARCHAR(100),
+  hiring_date DATE NOT NULL DEFAULT CURRENT_DATE,
+  notes TEXT
+);
+
+CREATE TABLE IF NOT EXISTS snacks (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  price DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+  id SERIAL PRIMARY KEY,
+  table_number INT NOT NULL,
+  order_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  status VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS stock_ingredients (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  category VARCHAR(50),
+  quantity INT NOT NULL DEFAULT 0
+);
