82f65c234f6ae73cecc7a0df5830c47accfb24ce
ViniciusLaureano <viniciusglaureano@gmail.com>
2026-03-18 19:45:54 -0300

    feat(react): aula 13

--- React/Fundamentos/11-dados-dinamicos-jsx/src/components/Subtitle.jsx
+++ React/Fundamentos/11-dados-dinamicos-jsx/src/components/Subtitle.jsx
@@ -2,4 +2,4 @@ function sum(a, b) {
 	return a + b;
 }
 
-export const Subtitle = () => <h2>It's easy, like 1 + 1 is {sum(1, 1)}</h2>;
+export const Subtitle = () => <h2 style={{ color: '#e5e5e5' }}>It's easy, like 1 + 1 is {sum(1, 1)}</h2>;
